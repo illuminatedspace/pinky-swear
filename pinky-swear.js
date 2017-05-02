3e5078045caecffb710af0fcabfab78906cafc6a
Liz <lizkristinaphillips@gmail.com>
2017-05-02 18:03:53 -0400

    moves text files to their own directory. implements promises that return and don't return the invocation of another promise. adds a bufferToStringPromise method that converts the readfile result to string. refactors the errors thrown to remove the extra 'Error:'s

--- pinky-swear.js
+++ pinky-swear.js
@@ -7,25 +7,59 @@ function readFilePromise (filePath) {
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, function (err, file) {
       if (err) {
-        console.log('ERROR: There\'s been a grevious error')
-        return reject(err)
+        console.error('There\'s been a grevious error')
+        return reject(new Error('there\'s no file there'))
       }
       resolve(file)
     })
   })
 }
 
+//promisified toString console log
+function bufferToStringPromise (buffer) {
+  return new Promise(function (resolve, reject) {
+    if (!buffer.length) {
+      console.error('Doesn\'t look like much of anything to me...')
+      return reject(new Error('buffer empty!'))
+    }
+    resolve(buffer.toString('utf8'))
+  })
+}
+
+
 //what a promise fulfilled to resolve sucessfully looks like
 //will read Antigone and console log the buffer
-/*readFilePromise('antigone.txt')
-  .then(result => {console.log(result.toString('utf8').slice(49547, 49719))})
+/*readFilePromise('./text-files/antigone.txt')
+  .then(result => {
+    console.log(result.toString('utf8').slice(49547, 49719))
+  })
   .catch(console.error)*/
 
 //a promise fulfilled to reject with an unhandled error (no .catch())
 /*  readFilePromise('notAFile.txt')
   .then(result => {console.log(result)})*/
 
-//a promise fulfilled to reject with an handled errors (with .catch())
-  readFilePromise('notAFile.txt')
+//a promise fulfilled to reject with a handled error (with .catch())
+/*  readFilePromise('notAFile.txt')
   .then(result => {console.log(result)})
+  .catch(console.error)*/
+
+//a promise that returns another promise
+/*readFilePromise('./text-files/antigone.txt')
+  .then((antigoneBuffer) => {
+    return bufferToStringPromise(antigoneBuffer)
+  })
+  .then((antigoneText) => console.log(antigoneText.slice(0, 100)))
+  .catch(console.error)*/
+
+//a promise that invokes but doesn't return another promise
+readFilePromise('./text-files/antigone.txt')
+  .then((antigoneBuffer) => {
+    bufferToStringPromise(antigoneBuffer)
+  })
+  .then((antigoneText) => console.log(antigoneText.slice(0, 100)))
   .catch(console.error)
+
+//a promise that returns a synchronus value
+
+//a promise that throws a synchronus error
